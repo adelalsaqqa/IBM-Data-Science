83dacd41efd89e404503532d72eeccf4ef5d00ed
adelalsaqqa <45515621+adelalsaqqa@users.noreply.github.com>
2023-07-20 21:54:10 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# <b>Objectives:</b>
+# * Understanding Common Data Science Tools
+# * Understanding Various Libraries in Data Science and API Concepts for REST Requests and Responses
+# * Exploring Interactive Computing with Jupyter
+# * R Programming and Data Manipulation
+# * Version Control and Collaboration
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1) Python
+# 
+# 2) R
+# 
+# 3) Julia
+# 
+# 4) Scala
+# 
+# 5) MATLAB
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1) NumPy
+# 2) pandas
+# 3) scikit-learn
+# 4) TensorFlow
+# 5) SciPy
+
+# Some popular open-source tools used in data science development:
+# <br><br>
+# 
+# <div style="margin-left: -800px;">
+#     
+# | <div style="text-align:center;"> Data Science Tools </div>|
+# |-------------------------------------------------------------------|
+# | <div style="text-align:center;">Jupyter Notebook/JupyterLab</div>|
+# |<div style="text-align:center;">RStudio</div>|
+# |<div style="text-align:center;">Apache Spark</div>|
+#     
+# </div>
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+(3 * 4) + 5
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# ## Author
+# Adel AlSaka
+
+# In[ ]:
+
+
+
+
+
+
+
+
+
+
+
